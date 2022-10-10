902028a0a247b9a72c77c6ec8829dc259c52855e
Austin Ofor <kwadoskii@live.com>
2022-10-10 09:56:53 +0100

    setup

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Octoba Challenge</title>
+      </Head>
+
+      <p>Hello world</p>
+    </>
+  );
+}
